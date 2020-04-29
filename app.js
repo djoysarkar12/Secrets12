48670c52f2b7ce271ebb417ca389a2c6161a92be
Dhananjoy Sarkar <JOY@JOY-SARKAR.COM>
2020-04-29 13:05:44 +0530

    Add Environment Vars

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
+
  
+
+
 
 userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
